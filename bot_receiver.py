500329474bb9e2e1aed3ecc374d167ea06471010
DimytroKashiroff <117631621+DimytroKashiroff@users.noreply.github.com>
2025-06-03 14:24:00 +0300

    Update bot_receiver.py

--- bot_receiver.py
+++ bot_receiver.py
@@ -1,4 +1,18 @@
-# bot_receiver.py
-# Telegram/Discord Bot no2 - reads messages from RabbitMQ and sends them ro the user
+import pika
 
-print("Bot Receiver running...")
+# Функція для обробки повідомлень
+def callback(ch, method, properties, body):
+    print(f"[x] Received: {body.decode()}")
+
+# Підключення до RabbitMQ
+connection = pika.BlockingConnection(pika.ConnectionParameters('localhost'))
+channel = connection.channel()
+
+# Створення черги (такої ж, як у sender)
+channel.queue_declare(queue='lab7_queue')
+
+# Очікування повідомлень
+channel.basic_consume(queue='lab7_queue', on_message_callback=callback, auto_ack=True)
+
+print("[*] Waiting for messages. To exit press CTRL+C")
+channel.start_consuming()
