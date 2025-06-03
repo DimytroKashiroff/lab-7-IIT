2cd44d3504bd4d21cabc25337150bbadbbb60078
DimytroKashiroff <117631621+DimytroKashiroff@users.noreply.github.com>
2025-06-03 15:16:39 +0300

    Update bot_receiver.py

--- bot_receiver.py
+++ bot_receiver.py
@@ -1,18 +1,31 @@
 import pika
+import asyncio
+from telegram import Bot
 
-# Функція для обробки повідомлень
-def callback(ch, method, properties, body):
-    print(f"[x] Received: {body.decode()}")
+bot_token = "ТВОКЕН_ТУТ"
+chat_id = "ID_КОРИСТУВАЧА_ТУТ"  # Отримай його вручну або через бота
 
-# Підключення до RabbitMQ
-connection = pika.BlockingConnection(pika.ConnectionParameters('localhost'))
-channel = connection.channel()
+bot = Bot(token=bot_token)
 
-# Створення черги (такої ж, як у sender)
-channel.queue_declare(queue='lab7_queue')
+# Отримати повідомлення з RabbitMQ
+def get_messages_from_rabbitmq():
+    connection = pika.BlockingConnection(pika.ConnectionParameters('localhost'))
+    channel = connection.channel()
+    channel.queue_declare(queue='lab7_queue')
 
-# Очікування повідомлень
-channel.basic_consume(queue='lab7_queue', on_message_callback=callback, auto_ack=True)
+    method_frame, header_frame, body = channel.basic_get(queue='lab7_queue', auto_ack=True)
+    connection.close()
+    return body.decode() if body else None
 
-print("[*] Waiting for messages. To exit press CTRL+C")
-channel.start_consuming()
+# Цикл перевірки кожні 15 хв
+async def check_queue():
+    while True:
+        message = get_messages_from_rabbitmq()
+        if message:
+            await bot.send_message(chat_id=chat_id, text=f"Викладач {message} почав віддалену пару")
+        await asyncio.sleep(15 * 60)  # кожні 15 хвилин
+
+# Запуск
+if __name__ == '__main__':
+    print("Bot No2 is running...")
+    asyncio.run(check_queue())
